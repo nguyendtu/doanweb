6a8ec0cba4278d6101c0c6f5a60a4753edc5c699
nguyendtu <duytu.nguyen@outlook.com>
2016-05-29 17:14:41 +0700

    added phone directive & main.css files

--- MobileApp/public/javascript/controllers/HomeController.js
+++ MobileApp/public/javascript/controllers/HomeController.js
@@ -2,16 +2,41 @@
 
     $scope.$broadcast("$stateChangeStart");
 
-    $scope.test = "Hello";
+    $scope.phones = [
+        {
+            name: "IPHONE SE 16GB",
+            image: "/public/images/IPHONE_SE_16GB.jpg",
+            price: "11.400.900",
+            description: "Dien thoai iphone se 16GB"
+        },
+        {
+            name: "IPHONE SE 16GB",
+            image: "/public/images/IPHONE_SE_16GB.jpg",
+            price: "11.400.900",
+            description: "Dien thoai iphone se 16GB"
+        },
+        {
+            name: "IPHONE SE 16GB",
+            image: "/public/images/IPHONE_SE_16GB.jpg",
+            price: "11.400.900",
+            description: "Dien thoai iphone se 16GB"
+        },
+        {
+            name: "IPHONE SE 16GB",
+            image: "/public/images/IPHONE_SE_16GB.jpg",
+            price: "11.400.900",
+            description: "Dien thoai iphone se 16GB"
+        }
+    ];
 
-    $http({
-        method: 'GET',
-        url: '/api/test/5'
-    }).then(function successCallback(response) {
-        $scope.test = response.data;
-    }, function errorCallback(response) {
-        $scope.test = "error";
-        console.log(response);
-    });
+    //$http({
+    //    method: 'GET',
+    //    url: '/api/test/5'
+    //}).then(function successCallback(response) {
+    //    $scope.test = response.data;
+    //}, function errorCallback(response) {
+    //    $scope.test = "error";
+    //    console.log(response);
+    //});
 
 }]);
---
+++ MobileApp/public/javascript/directives/products.js
@@ -0,0 +1,19 @@
+﻿mobileApp.directive("phone", function () {
+
+    var link = function() {
+        $scope.phones = phone;
+
+        console.log(phone);
+
+    };
+
+
+    return {
+        restrict: 'E',
+        templateUrl: '/public/views/phone/phone.html',
+        scope: {
+            phones: '='
+        },
+        link: 'link'
+    };
+});
